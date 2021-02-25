5d43c8251f61c278f1cdd8314c9f5454f06450cf
exoad <45053009+exoad@users.noreply.github.com>
2021-02-24 20:30:18 -0500

    added this main file

---
+++ app.js
@@ -0,0 +1,29 @@
+const { Client, Collection, Discord } = require('discord.js');
+const bot = new Client();
+
+const { prefix } = require(`./config.json`);
+[`aliases`, `commands`].forEach(x => bot[x] = new Collection());
+["command", "events"].forEach(x => require(`./handlers/${x}`)(bot));
+
+bot.on('message', message => {
+  if (message.content == `<@${bot.user.id}>` || message.content == `<@!${bot.user.id}>`)
+    return message.channel.send(`My prefix is ${prefix}`);
+
+  if (message.content == `${prefix}highlight`) {
+    let rety = message.content.split(" ");
+    if (!rety || rety == undefined) {
+      message.channel.send("Invalid Characters")
+    }
+    message.channel.send("```" + rety + "```");
+  }
+  if (message.content.includes(`senpai`) && message.guild.id == '792194075599568926') {
+    message.delete();
+    message.reply('bad word, thats a no no :|').then(m => {
+      m.delete({ timeout: 5000 });
+    })
+  }
+})
+
+
+const { TOKEN } = require('./config.json');
+bot.login(`Nzk4NjcwMTE3OTc0MDQ4ODE5.X_4Zgg.ROnvrH950ZXc3CdPdj9u28XW97c`);
