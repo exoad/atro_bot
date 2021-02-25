5af82122afbbd9dcb48454ebee1291c97cdec0a9
exoad <45053009+exoad@users.noreply.github.com>
2021-02-24 21:06:51 -0500

    Update and rename deprecation_pending.class to deprecation_pending.java

---
+++ assets/deprecation_pending.java
@@ -0,0 +1,14 @@
+/**
+ * This is a deprecation File
+*/
+class deprecation_pending extends atro_demarkation {
+  private String modelName = "Node :1";    // Car attribute
+  public static void main(String[] args) {
+
+    deprecation_pending newDemark = new deprecation_pending();
+
+     //...
+
+    System.out.printf();
+  }
+}
