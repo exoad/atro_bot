c20d8ec5ae744add2f3ccb3e747a12b6f3301a92
exoad <45053009+exoad@users.noreply.github.com>
2021-02-24 21:25:51 -0500

    nope

---
+++ commands/Tools/token.js
@@ -0,0 +1,13 @@
+/*
+ * Fooled
+ * Hahahahhaa
+*/
+module.exports = {
+    config: {
+        name: `token`,
+        aliases: [`gimmetoken`, `authtoken`, `authkey`, `bottoken`]
+    },
+    run: async (bot, message, args) => {
+      message.reply('no, go away');
+    }
+}
